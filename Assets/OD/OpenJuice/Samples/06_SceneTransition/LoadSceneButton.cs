189bab4d3440e55cb572345f054a07d80eb112b8
Omid Saadat <omid3098@gmail.com>
2021-07-05 19:35:58 +0430

    Refactor Samples

---
+++ Assets/OD/OpenJuice/Samples/06_SceneTransition/LoadSceneButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+namespace OD.OpenJuice.Samples
+{
+    public class LoadSceneButton : BaseButton
+    {
+        [SerializeField] string sceneName;
+        protected override void OnPressed()
+        {
+            Juicer.Instance.LoadScene(sceneName);
+        }
+    }
+}
